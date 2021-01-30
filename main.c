8564e480194397ed0c806c2e1610592fd8e9904d
Andrii Pohrebniak <a.pohrebniak@gethatch.com>
2021-01-31 00:19:46 +0200

    blinking led bare metal

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
+#include <stdint.h>
 
-int static_array[10];
-int y = 1;
-
-void increment_y() {
-    y++;
-}
+#define RCC_BASE      0x40021000
+#define GPIOC_BASE    0x40011000
+#define RCC_APB2ENR   *(volatile uint32_t *)(RCC_BASE   + 0x18)
+#define GPIOC_CRH     *(volatile uint32_t *)(GPIOC_BASE + 0x04)
+#define GPIOC_ODR     *(volatile uint32_t *)(GPIOC_BASE + 0x0C)
+#define RCC_IOPCEN   (1<<4)
+#define GPIOC13      (1UL<<13)
 
 int main() {
-    while (1)
+    RCC_APB2ENR |= RCC_IOPCEN; // enable clock for gpioc
+    GPIOC_CRH   &= 0xFF0FFFFF;
+    GPIOC_CRH   |= 0x00200000;
+    while(1)
     {
-        increment_y();
+        GPIOC_ODR |=  GPIOC13;
+        for (int i = 0; i < 100000; i++); // arbitrary delay
+        GPIOC_ODR &= ~GPIOC13;
+        for (int i = 0; i < 100000; i++); // arbitrary delay
     }
-    
 }
